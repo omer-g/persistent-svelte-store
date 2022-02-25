866f223044a616dc994b8baf0f7ed90174f7671b
OD <omer.d@campus.technion.ac.il>
2022-02-25 22:03:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# persistent-svelte-store
+
+## Description
+A generic persistent writable store, built from scratch in TypeScript according to the Svelte store contract. Store value is stored in `localStorage` as a JSON string, but this is transparent to user.
+
+## Example
+Create a store and supply a type
+>`stores.ts`
+>```typescript
+> import { persistentWritable } from "./persistentStore";
+> export const store = persistentWritable<object>("storeKey", {});
+Use like any writable store
+>`App.svelte`
+>```typescript
+> <script lang="ts">
+>   import { store } from "./stores";
+>   $store = { id: 1 };
+>   console.log($store.id);
+> </script>
+
+## Sources
+To learn more about Svelte stores see the [Svelte Docs](https://svelte.dev/docs).
---
+++ persistentStore.ts
@@ -0,0 +1,49 @@
+/** -------------------------- persistentStore.ts -------------------------- */
+
+/** A generic persistent store according to the Svelte store contract
+ * 
+ *  @example
+ *      import { persistentWritable } from "./persistentStore";
+ *      export const store = persistentWritable<object>("storeKey", {});
+ * 
+ *      $store = { id: 1 };
+ *      console.log($store.id);
+ * 
+ *  @template T - Should be a type JSON.stringify can process
+ *  @param {string} storeKey - A key in localStorage for the store
+ *  @param {T} initialValue - Initial value of store
+ */
+export const persistentWritable = <T>(storeKey: string, initialValue: T) => {
+  let subscriptions: ((value: T) => void)[] = [];
+  let storeValue: T;
+  
+  let currentStoreString = localStorage.getItem(storeKey);
+  if (currentStoreString === null || currentStoreString === undefined) {
+    storeValue = initialValue;
+    localStorage.setItem(storeKey, JSON.stringify(storeValue));
+  } else {
+    storeValue = JSON.parse(localStorage.getItem(storeKey));
+  }
+
+  // Subscribes function and returns an unsubscribe function
+  const subscribe = (subscription: (value: T) => void) => {
+    subscription(storeValue);
+    subscriptions = [...subscriptions, subscription];
+    const unsubscribe = () => {
+      subscriptions.filter(s => s != subscription);
+    }
+    return unsubscribe;
+  }
+
+  // Sets stringified value in local storage and calls subscriptions
+  const set = (value: T) => {
+    storeValue = value;
+    localStorage.setItem(storeKey, JSON.stringify(value));
+    subscriptions.forEach(subscription => subscription(storeValue));
+  }
+
+  // Updates store value according to input function
+  const update =
+    (update_func: (curr: T) => T) => set(update_func(storeValue));
+  return { subscribe, set, update };
+}
